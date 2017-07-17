5bbd841dcf8f783e922aba546bb2d73791ba76d2
Fang <funkapunkafide@gmail.com>
2017-07-17 14:44:38 -0500

    made media query for responsive design

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,15 +8,14 @@ function cards(name, url, description, price) {
 
 //===============Object Constructed=====================//
 var cardobject = [];
-cardobject.push(new cards("a", "images/air-assortment.jpg", "fd", 50));
-cardobject.push(new cards("a", "images/jar-in-snow.jpg", "fd", 50));
-cardobject.push(new cards("a", "<img src='asdfasdf.png'>", "fd", 50));
-cardobject.push(new cards("a", "<img src='asdfasdf.png'>", "fd", 50));
-cardobject.push(new cards("a", "<img src='asdfasdf.png'>", "fd", 50));
-cardobject.push(new cards("a", "<img src='asdfasdf.png'>", "fd", 50));
-cardobject.push(new cards("a", "<img src='asdfasdf.png'>", "fd", 50));
-cardobject.push(new cards("a", "<img src='asdfasdf.png'>", "fd", 50));
-
+cardobject.push(new cards("Air We There Yet", "images/mainjar.png", "Lorem1 ipsum doloe inim vecitation ullamco laboris nisi ut aliquip ollit anim id est laborum.", 500));
+cardobject.push(new cards("Air-O-Matic", "images/jar2.png", "Lorem2 ipsum dolor sadiam, quis nostr laboris nisi ut aliquip ex ea commodo conseit in vololore eu fugiat nulla pariatborum.", 600));
+cardobject.push(new cards("Bad Air Day", "images/jar3.png", "Lorem3 ipsum dolor s, sed dgna ali quis nostrud exercitation ullamco laboris nisi ut aauten voluptate velit esseExcepteur  mollit anim id est laborum.", 700));
+cardobject.push(new cards("Air-O-Space", "images/jar4.png", "Lorem4 ipsum dolor o.boris nisi o consequat. Duis aute irure dolor in reprehenderit in vour. Excepteur sint occaecat cupidatat non proimollit anim id est laborum.", 800));
+cardobject.push(new cards("Beach Air", "images/jar2.png", "Lorem5 ipsum dolor sitincididunqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex  e eu fuupidatalit anim id est laborum.", 1000));
+cardobject.push(new cards("Espresso in the Air", "images/jar4.png", "Lorem6 ipsum dolor sitdo e ut labullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reest laborum.", 1100));
+cardobject.push(new cards("Margin of Air-r", "images/jar3.png", "Lorem7 ipsum docididunt ut labo ad minim ve commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit eest laborum.", 1200));
+cardobject.push(new cards("Share some Air", "images/mainjar.png", "Lorem8 ipsum dolididunaliquarudabohenderit ite velit esse cillum dolore eu fugiat nulla pariatur. Excepteurit ast laborum.", "All the monies"));
 
 //===============Printing Object to HTML=====================//
 
@@ -34,15 +33,15 @@ cardobject.push(new cards("a", "<img src='asdfasdf.png'>", "fd", 50));
 cardobject.forEach(function(item){
   document.getElementById("cards").innerHTML += `<div class="card">
       <h2>${item.Name}</h2>
-      <a href="${item.Url}"><img src="${item.Url}"></a>
-      <p class="caption">${item.Description}</p>
+      <a href="${item.Url}"><img src="${item.Url}" alt="${item.Description}"></a>
+      <p class="caption">${item.Description}</p><br>
       <p class="price">$${item.Price}</p>
       <button><a>Purchase</a></button>
     </div>`;
   
 });
 
-
+//===============Making Images Clickable with Overlay=====================//
 
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
@@ -68,7 +67,7 @@ $(".card img").click(function(event){
   $overlay.show();
   
   //Get child's title attribute and set caption
-  var captionText = $(".caption").html();
+  var captionText = $(this).attr("alt");
   $caption.text(captionText);
 });
 
